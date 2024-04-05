447d3cb68e926a9432546bef4151205e3661de61
YujiAsai(浅井有志) <yuji.asai.a845@dirbato.co.jp>
2024-04-05 17:13:35 +0900

    新規

---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("kiramex");
+        System.out.println("Java");
+        System.out.println("学修を始めましょう！");
+    }
+}
